93569cd74ce233694154ff14cf9541800f7dc0b9
Aviral Kumar <aviralkum@gmail.com>
2017-11-17 14:36:27 +0530

    Background image added in Mainpage

--- student_assistant_client/src/components/MainPage.js
+++ student_assistant_client/src/components/MainPage.js
@@ -6,6 +6,7 @@ import Login from './Login';
 import Message from './Message';
 import User from './User';
 import dropboxIcon from '../images/dropbox.png'
+import sjsu from '../images/sjsu.JPG'
 
 class MainPage extends Component {
 
@@ -157,8 +158,8 @@ class MainPage extends Component {
                             <div className="container-fluid">
                                 <div className="col-lg-12 col-xs-12 col-md-12 col-sm-12">
                                     <div className="row">
-                                        <div className= "col-lg-1 col-md-1 col-xs-1 col-sm-1">
-                                            <img src={dropboxIcon} width="50" height="50" alt="DropBox"/>
+                                        <div className= "col-lg-1 col-md-1 col-xs-1 col-sm-1" id="logo">
+                                            <img src={sjsu} width="100" height="100" alt="DropBox"/>
                                         </div>
                                     </div>
                                     <div className="row">
