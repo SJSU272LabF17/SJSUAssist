3b01b362d36bc68dce4a855a3373b56b7c327378
varun1524 <varun3273@gmail.com>
2017-12-08 15:09:27 -0800

    skillset and userissues added

--- student_assistant_client/src/reducers/index.js
+++ student_assistant_client/src/reducers/index.js
@@ -2,13 +2,16 @@
 import {combineReducers} from 'redux';
 import Openissues from './openissues-reducer';
 import Resolveissue from './resolveisseues - reducer'
+import skillset from './setskills_reducer';
+import handleUserIssues from './userissues_reducer';
 
 const allReducers = combineReducers({
 
     //insert reducer name here to combine
-    openissues :Openissues,
-    resolveissue:Resolveissue
-
+    openissues : Openissues,
+    resolveissue : Resolveissue,
+    skillset : skillset,
+    userIssues : handleUserIssues
 });
 
 export default allReducers;
