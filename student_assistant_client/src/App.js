d8a4f815fc2c948099da29d9048d59e8f7399739
varun1524 <varun3273@gmail.com>
2017-12-08 15:13:18 -0800

    stylesheet reference given to latest bootstrap

--- student_assistant_client/src/App.js
+++ student_assistant_client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"/>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"/>
                 <MainPage/>
             </div>
         </BrowserRouter>
